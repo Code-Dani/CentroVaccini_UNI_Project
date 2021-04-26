b075a38b97f391c31c6a080bee889f4eedcbb2ca
Mene-hub <menegotto.claudio@gmail.com>
2021-04-26 14:28:33 +0200

    aggiunta classi EventoAvverso e Utente
    ho aggiunto nel programma cittadino la classe Utente ed EventoAvverso

---
+++ PortaleCittadini/src/EventoAvverso.java
@@ -0,0 +1,41 @@
+/** classe per la definizione di un evento avvesro*/
+public class EventoAvverso
+{
+    /** definisce il tipo di EventoAvverso verificato post vaccino*/
+    public enum Evento
+    {
+        mal_di_testa,
+        febbre,
+        dolori_muscolari_e_articolari,
+        infoadonopatia,
+        tachicardita,
+        crisi_ipertensiva,
+        altro
+    }
+
+    /** definisce la gravità con la quale si verifica EventoAvverso post vaccino*/
+    public enum Severita
+    {
+        molto_bassa_1,
+        bassa_2,
+        fastidiosa_3,
+        sopportabile_4,
+        insopportabile_5
+    }
+
+    public Evento evento;
+
+    public Severita severita;
+
+    /** per l'inserimento delle note opzionali inserite dall'utente. di massimo 255 caratteri*/
+    //da vedere se tenere stringa o array di char
+    public String noteOpzionali;
+
+    /** costruttore*/
+    public EventoAvverso(Evento _evento, Severita _severita, String _noteOpzionali)
+    {
+        evento = _evento;
+        severita = _severita;
+        noteOpzionali = _noteOpzionali;
+    }
+}
---
+++ PortaleCittadini/src/Utente.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+
+/**classe per la definizione dell'utente registrao*/
+public class Utente {
+    public String nome;
+    public String cognome;
+    public String codiceFiscale;
+    public String indirizzoEmail;
+    private String userID;
+    private String password;
+
+    /**id vacinazione univoco su 16bit*/
+    public short idVaccinazione;
+
+    /**enum Vaccino per la definizione dei vaccini somministrabili*/
+    public enum Vaccino
+    {
+        Pfizer,
+        AstraZeneca,
+        Moderna,
+        JeJ{
+            public String toString(){
+                return "J&J";
+            }
+        }
+    }
+
+    public Vaccino vaccino;
+
+    /** lista di eventi avversi dell'utente*/
+    public ArrayList<EventoAvverso> eventiAvversi;
+
+    /** costruttore*/
+    public Utente (){
+        eventiAvversi  = new ArrayList<EventoAvverso>();
+    }
+
+    /**aggiunge alla lista gli eventi avversi inseriti dagli utenti e vengono salvati su file*/
+    public void inserisciEventiAvversi(){
+        //da gestire con la parte grafica e salvataggio in file
+    }
+}
