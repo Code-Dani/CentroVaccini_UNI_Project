46f81adf407d7730b0e97ecdfdae6f3396cf73b4
Mene-hub <menegotto.claudio@gmail.com>
2021-04-27 11:35:43 +0200

    mini fix

--- PortaleCittadini/src/CentroVaccinale.java
+++ PortaleCittadini/src/CentroVaccinale.java
@@ -33,12 +33,12 @@ public class CentroVaccinale
      * @param tipologia tipologia del Centro Vaccinale
      * @param idVaccinazione chiave esterna per poter cercare le vaccinazioni effettuate ai clienti registrati al centro
      */
-    public CentroVaccinale(String nome, Indirizzo indirizzo,Tipologia tipologia, ArrayList<Short> idVaccinazione)
+    public CentroVaccinale(String _nome, Indirizzo _indirizzo,Tipologia _tipologia, ArrayList<Short> _idVaccinazione)
     {
-        this.nome=nome;
-        this.indirizzo=indirizzo;
-        this.tipologia=tipologia;
-        this.idVaccinazione=idVaccinazione;
+        nome = _nome;
+        indirizzo = _indirizzo;
+        tipologia = _tipologia;
+        idVaccinazione = _idVaccinazione;
     }
 
     /**
