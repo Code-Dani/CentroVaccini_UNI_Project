abfc852a6dc5a97c1de66e7081395e4ecc976efd
Code-Dani <satrianodaniel@gmail.com>
2021-05-01 00:07:21 +0200

    Aggiustato la window in portale cittadini
    (nulla di che, cambiato due numeri)

--- PortaleCittadiniFX/src/sample/Main.java
+++ PortaleCittadiniFX/src/sample/Main.java
@@ -11,13 +11,17 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("Portale Cittadini");
+        primaryStage.setScene(new Scene(root, 1280, 720));
         primaryStage.show();
+
+
     }
 
 
     public static void main(String[] args) {
         launch(args);
+
+
     }
 }
