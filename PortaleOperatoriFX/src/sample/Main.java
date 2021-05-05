27b4484bbd5b2a9909d13defac7784812025a5bf
Code-Dani <satrianodaniel@gmail.com>
2021-05-05 18:22:36 +0200

    Aggiunta classi grafiche
    Co-Authored-By: Mene-hub <72011313+Mene-hub@users.noreply.github.com>

--- PortaleOperatoriFX/src/sample/Main.java
+++ PortaleOperatoriFX/src/sample/Main.java
@@ -10,9 +10,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 700, 700));
+        Parent root = FXMLLoader.load(getClass().getResource("MainWindow.fxml"));
+        primaryStage.setTitle("Portale Operatori Vaccinali");
+        primaryStage.setScene(new Scene(root, 1280, 720));
         primaryStage.show();
     }
 
