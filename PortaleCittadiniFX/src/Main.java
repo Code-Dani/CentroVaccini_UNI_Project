0e3917714c4f07e31b4fa55c00953d600602d220
Mene-hub <menegotto.claudio@gmail.com>
2021-07-18 21:08:13 +0200

    margini brutti
    xd lol

--- PortaleCittadiniFX/src/Main.java
+++ PortaleCittadiniFX/src/Main.java
@@ -4,14 +4,25 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("/FXML/Home.fxml"));
+        /*Parent root = FXMLLoader.load(getClass().getResource("/FXML/Home.fxml"));
         primaryStage.setTitle("Portale Cittadini");
         primaryStage.setScene(new Scene(root));
+        primaryStage.show();*/
+
+        Parent root = FXMLLoader.load(getClass().getResource("/FXML/Home.fxml"));
+        primaryStage.setTitle("Portale Cittadini");
+
+        primaryStage.initStyle(StageStyle.UNDECORATED);
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+
+        primaryStage.setFullScreenExitHint("Premi ESC per uscire dalla modalità a tutto schermo");
         primaryStage.show();
 
 
