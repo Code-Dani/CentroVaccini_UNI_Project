b7d41199a13bf3eadee9a18b8fa800c067cad71a
cristiandenicola <cristiandenicola009@gmail.com>
2021-09-03 18:32:06 +0200

    Login e registrazione
    bella pe me che è da ieri che l'ho fatto e non ho pushato :,)

---
+++ PortaleCittadiniFX/src/Controllers/Login.java
@@ -0,0 +1,38 @@
+package Controllers;
+
+import com.jfoenix.controls.JFXButton;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+
+public class Login {
+
+    @FXML
+    private ImageView IMG_reduce;
+
+    @FXML
+    private ImageView IMG_restoredown;
+
+    @FXML
+    private ImageView IMG_exit;
+
+    @FXML
+    private PasswordField PFpassword;
+
+    @FXML
+    private TextField TxtNUtente;
+
+    @FXML
+    private JFXButton BtnLogin;
+
+    @FXML
+    private Label LblReindirizzamento;
+
+    /*@FXML
+    void window_status(MouseEvent event) {
+
+    }*/
+
+}
---
+++ PortaleCittadiniFX/src/Controllers/Registrazione.java
@@ -0,0 +1,41 @@
+package Controllers;
+
+import com.jfoenix.controls.JFXButton;
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+
+public class Registrazione {
+
+    @FXML
+    private ImageView IMG_reduce;
+
+    @FXML
+    private ImageView IMG_restoredown;
+
+    @FXML
+    private ImageView IMG_exit;
+
+    @FXML
+    private PasswordField PFpassword;
+
+    @FXML
+    private TextField TxtNome;
+
+    @FXML
+    private TextField TxtCognome;
+
+    @FXML
+    private TextField TxtNUtente;
+
+    @FXML
+    private JFXButton BtnRegistrazione;
+
+    /*@FXML
+    void window_status(MouseEvent event) {
+
+    }*/
+
+}
+
