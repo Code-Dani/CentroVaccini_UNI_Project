aba43d7b4ed66dcaf949180ed6bcbfc6d6dd9d13
Code-Dani <satrianodaniel@gmail.com>
2022-08-22 14:19:44 +0200

    Sistemata data nella javaDoc

--- PortaleOperatoriFX/src/interfaces/OperatoriMethods.java
+++ PortaleOperatoriFX/src/interfaces/OperatoriMethods.java
@@ -35,7 +35,7 @@ public interface OperatoriMethods extends Remote {
     /**
      * Metodo utilizzato per recuperare tutti i centri vaccinali registrati
      * @author Daniel Satriano
-     * @since 8/08/2022
+     * @since 22/08/2022
      * @return lista di centri vaccinali List<CentroVaccinale>
      * @throws RemoteException eccezione sollevata nel caso la connessione con il server RMI non dovesse andare a buon fine
      */
@@ -44,7 +44,7 @@ public interface OperatoriMethods extends Remote {
     /**
      * Metodo utilizzato per recuperare tutti gli utenti vaccinati, utilizzato per lo storico
      * @author Daniel Satriano
-     * @since 8/08/2022
+     * @since 22/08/2022
      * @return Ritorna una lista di utenti vaccinati
      * @throws RemoteException eccezione sollevata nel caso la connessione con il server RMI non dovesse andare a buon fine
      */
